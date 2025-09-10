afab389af895de6ac8951c8364668c72e3819a8c
Dineshk2219684 <100016846+Dineshk2219684@users.noreply.github.com>
2025-09-10 21:40:44 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 26851879
+API_HASH = "aac0cd7d51dfc91c5bc70c26763134bf"
+BOT_TOKEN = "8040496781:AAGsjQ8Yj9KCI6g3p-ywahObkaabu6ltKaE"
+SESSION config("SESSION", default=None)
+FORCESUB config("FORCESUB", default=None)
+AUTH = 8245545215
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
